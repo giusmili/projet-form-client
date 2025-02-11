536ad6932cd0df5beb8dc40334ca3f3d04d27f4c
giusmili <giusmili67@gmail.com>
2025-02-11 15:55:42 +0100

    js

--- src/app.js
+++ src/app.js
@@ -1,48 +1,34 @@
-document.addEventListener("DOMContentLoaded",(e)=>{
-        e.preventDefault()
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault();
 
-        const inputName = document.querySelectorAll('input[type="text"]')[0]
-        const inputLastName = document.querySelectorAll('input[type="text"]')[1]
-        const age = document.getElementsByTagName("input")[2]
-        const btn_submit = document.getElementsByTagName('input')[3]
+  const inputName = document.querySelectorAll('input[type="text"]')[0];
+  const inputLastName = document.querySelectorAll('input[type="text"]')[1];
+  const age = document.getElementsByTagName("input")[2];
+  const btn_submit = document.getElementsByTagName("input")[3];
 
-        /* target */
+  /* target */
 
-        const target = document.querySelector(".msg")
-      
-     
+  const target = document.querySelector(".msg");
 
-        btn_submit.addEventListener("click",(e)=>{
-            e.preventDefault()
+  btn_submit.addEventListener("click", (e) => {
+    e.preventDefault();
 
-            if(inputName.value && inputName.value && age.value){
-                Swal.fire({
-                    title: "Good job!",
-                    text: "You clicked the button!",
-                    icon: "success"
-                  })
-                
-                
-                target.innerText = `Bonjour voici les infos saisies :  
-                ${inputName.value}, ${inputLastName.value}, ${age.value} ans`
-                  
-                 
+    if (inputName.value && inputName.value && age.value) {
+      Swal.fire({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success",
+      });
 
-            }
-           
-            else{
-                Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Something went wrong!",
-                    footer: 'Alert'
-                })
-              
-
-            }
-
-             
-
-        
-        })
-})
+      target.innerText = `Bonjour voici les infos saisies :  
+                ${inputName.value}, ${inputLastName.value}, ${age.value} ans`;
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: "Alert",
+      });
+    }
+  });
+});
