180ea4278cd3436703264ef53f7b8300dd5bcc12
giusmili <giusmili67@gmail.com>
2025-02-11 16:30:21 +0100

    js

--- src/app.js
+++ src/app.js
@@ -13,16 +13,15 @@ document.addEventListener("DOMContentLoaded", (e) => {
   btn_submit.addEventListener("click", (e) => {
     e.preventDefault();
 
-    if (inputName.value && inputName.value && age.value) {
+    if (inputName.value && inputLastName.value && age.value && age.value >= 16 ) {
       Swal.fire({
         title: "Good job!",
         text: "You clicked the button!",
         icon: "success",
       });
 
-        target.innerText = `Bonjour voici les infos saisies :  
-        ${inputName.value}, ${inputLastName.value}, ${age.value} ans`;
-        
+        target.innerText = `Bonjour voici les infos saisies : ${inputName.value}, ${inputLastName.value}, ${age.value} ans`;
+
     } else {
       Swal.fire({
         icon: "error",
