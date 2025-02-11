12660f91b85d58942b5d2db94462dd6eb9aa89a5
giusmili <giusmili67@gmail.com>
2025-02-11 17:41:45 +0100

    js support html

--- src/app.js
+++ src/app.js
@@ -2,41 +2,49 @@ document.addEventListener("DOMContentLoaded", (e) => {
   e.preventDefault();
 
   const btn_submit = document.getElementsByTagName("input")[3];
-  
-/* objet */
 
-  const userForm = ({
+  /* objet */
+
+  const userForm = {
     inputName: document.querySelectorAll('input[type="text"]')[0],
     inputLastName: document.querySelectorAll('input[type="text"]')[1],
     age: document.getElementsByTagName("input")[2],
-    target: document.querySelector(".msg")
-  })
+    target: document.querySelector(".msg"),
 
-
+    userWatch() {
+      if (
+        this.inputName.value &&
+        this.inputLastName.value &&
+        this.age.value &&
+        this.age.value >= 16 &&
+        !isNaN(this.age.value)
+      ) {
+        Swal.fire({
+          title: "Good job!",
+          text: "You clicked the button!",
+          icon: "success",
+        });
+        this.target.classList.remove("error");
+        this.target.classList.add("appear");
+        this.target.innerText = `Bonjour voici les infos saisies : ${this.inputName.value}, ${this.inputLastName.value}, ${this.age.value} ans`;
+        document.location.assign("dashboard.html")
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+          footer: "Alert",
+        });
+        this.target.classList.add("error");
+        this.target.classList.add("appear");
+        this.target.innerText = `Tous les champs sont obligatoires!!!`;
+      }
+    },
+  };
 
   btn_submit.addEventListener("click", (e) => {
     e.preventDefault();
 
-    if (userForm.inputName.value && userForm.inputLastName.value && userForm.age.value && userForm.age.value >= 16 && !isNaN(userForm.age.value)) {
-      Swal.fire({
-        title: "Good job!",
-        text: "You clicked the button!",
-        icon: "success",
-      });
-      userForm.target.classList.remove("error")
-      userForm.target.classList.add("appear")
-      userForm.target.innerText = `Bonjour voici les infos saisies : ${userForm.inputName.value}, ${userForm.inputLastName.value}, ${userForm.age.value} ans`;
-
-    } else {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Something went wrong!",
-        footer: "Alert",
-      });
-      userForm.target.classList.add("error")
-      userForm.target.classList.add("appear")
-      userForm.target.innerText = `Tous les champs sont obligatoires!!!`
-    }
+    userForm.userWatch();
   });
 });
